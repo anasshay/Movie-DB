020a330c7591046ed1c4a2b83752caab6e6329fd
anasshay <anasshaydar5@gmail.com>
2022-01-08 23:36:55 +0200

    Step 2

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("ok.");
-  });
+  res.send("ok.");
+});
 
-app.listen(port, console.log(`Connected to ${port}`))
+app.get("/test", (req, res) => {
+  res.status(200).send({ status: 200, message: "ok" });
+});
+app.get("/time", (req, res) => {
+  let time = new Date();
+  res
+    .status(200)
+    .send({ status: 200, message: `${time.getHours()}:${time.getMinutes()}` });
+});
+
+app.listen(port, console.log(`Connected to ${port}`));
